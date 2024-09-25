5505e773ddbe6ac3411fe011d6f5c3313deb5082
barbarerazmadze29 <nataliarzm4@gmail.com>
2024-09-25 18:27:59 +0400

    commit

---
+++ Level 27/homework/Task0.py
@@ -0,0 +1,9 @@
+List = ["banana", "tomato", 13, 245, 23.4, 12.6, True, False,88, 90]
+
+List[1]=34
+List[3]=56
+List[5]=767789
+List[7]='mango'
+List[9]='56,9'
+
+print(List)
---
+++ Level 27/homework/Task3.py
@@ -0,0 +1,3 @@
+numbers=[5, 6,27,97,2864,586,88]
+num2=[numbers[0] * numbers[1] *numbers[2] * numbers[3] *numbers[4] * numbers[5]]
+print(num2) 
