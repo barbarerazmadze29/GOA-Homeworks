44bd5b45133266f2feeee0df57f839ba8e254a05
barbarerazmadze29 <nataliarzm4@gmail.com>
2024-09-28 23:00:59 +0400

    commit

---
+++ Level 30/homework/Task2.py
@@ -0,0 +1,12 @@
+#დავალება: შექმენით ფუნქცია, რომელიც მომხმარებელს შეკითხება პაროლს, სანამ სოწორს არ ჩაწერს მომხმარებელი.
+
+password = "12345678"
+while True:
+        password1 = input("Enter your password: ")
+        if password == password1:
+            print("Correct!")
+            break
+        else:
+            print("Incorrect password. Try again.")
+
+
